26cf9b80e5054da9613d8c6cb7354d131461e1bc
Yumin <potapu51@gmail.com>
2024-04-03 09:57:02 +0900

    ASC를 반환받는 인터페이스 제작

---
+++ Source/UnrealPortfolio/Interface/AbilitySystemGetInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interface/AbilitySystemGetInterface.h"
+
+// Add default functionality here for any IAbilitySystemGetInterface functions that are not pure virtual.
