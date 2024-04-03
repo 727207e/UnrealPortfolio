b75cb061a3542ab2769b4e0462ce9c53fc3bc733
Yumin <potapu51@gmail.com>
2024-04-03 14:57:47 +0900

    컨트롤러와 캐릭터 사이의 인터페이스 구현

---
+++ Source/UnrealPortfolio/Interface/UPPossessCharacterInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UPPossessCharacterInterface.h"
+
+// Add default functionality here for any IUPPossessCharacterInterface functions that are not pure virtual.
---
+++ Source/UnrealPortfolio/Interface/UPPossessCharacterInterface.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "UPPossessCharacterInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UUPPossessCharacterInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class UNREALPORTFOLIO_API IUPPossessCharacterInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	virtual	void OnAttackStart() = 0;
+	virtual void OnSkillStart(int32 Index) = 0;
+	virtual void OnSkillRelease(int32 Index) = 0;
+	virtual void OnConsumableStart(int32 Index) = 0;
+	virtual void OnAvoidStart() = 0;
+	virtual void OnMenuStart() = 0;
+	virtual void OnInventoryStart() = 0;
+
+	virtual void OnInputStart() = 0;
+	virtual void OnSetDestinationTriggered() = 0;
+	virtual void OnSetDestinationReleased() = 0;
+};
