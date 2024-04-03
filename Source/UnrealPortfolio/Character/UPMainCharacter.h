02e89ebc9ee0fe38867c9f37322278c367428ee6
Yumin <potapu51@gmail.com>
2024-04-03 14:58:09 +0900

    메인 캐릭터 인터페이스로 구현

--- Source/UnrealPortfolio/Character/UPMainCharacter.h
+++ Source/UnrealPortfolio/Character/UPMainCharacter.h
@@ -6,13 +6,14 @@
 #include "Character/UPCharacter.h"
 #include "Abilities/GameplayAbilityTypes.h"
 #include "Interface/AbilitySystemGetInterface.h"
+#include "Interface/UPPossessCharacterInterface.h"
 #include "UPMainCharacter.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class UNREALPORTFOLIO_API AUPMainCharacter : public AUPCharacter, public IAbilitySystemGetInterface
+class UNREALPORTFOLIO_API AUPMainCharacter : public AUPCharacter, public IAbilitySystemGetInterface, public IUPPossessCharacterInterface
 {
 	GENERATED_BODY()
 	
@@ -22,6 +23,26 @@ public :
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
 	virtual void PossessedBy(AController* NewController) override;
+
+	virtual	void OnAttackStart() override;
+	virtual void OnSkillStart(int32 Index) override;
+	virtual void OnSkillRelease(int32 Index) override;
+	virtual void OnConsumableStart(int32 Index) override;
+	virtual void OnAvoidStart() override;
+	virtual void OnMenuStart() override;
+	virtual void OnInventoryStart() override;
+
+	virtual void OnInputStart() override;
+	virtual void OnSetDestinationTriggered() override;
+	virtual void OnSetDestinationReleased() override;
+	
+	/** Time Threshold to know if it was a short press */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	float ShortPressThreshold;
+
+	/** FX Class that we will spawn when clicking */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	TObjectPtr<class UNiagaraSystem> FXCursor;
 
 protected :
 	UPROPERTY(EditAnywhere, Category = GAS)
@@ -35,4 +56,9 @@ protected :
 
 private :
 	void SetupGasInput(AController* NewController);
+
+private :
+	//Movement
+	float FollowTime; // For how long it has been pressed
+	FVector CachedDestination;
 };
