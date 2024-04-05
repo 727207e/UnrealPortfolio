6e195bcaaab509f4eab565210658a12671f2060f
Yumin <potapu51@gmail.com>
2024-04-05 11:47:08 +0900

    LevelActor 제작
    맵 이동시 해당 로직을 탄다.

---
+++ Source/UnrealPortfolio/Level/UPLevelScriptActor.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/LevelScriptActor.h"
+#include "UPLevelScriptActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPORTFOLIO_API AUPLevelScriptActor : public ALevelScriptActor
+{
+	GENERATED_BODY()
+	
+public :
+	AUPLevelScriptActor();
+
+	UPROPERTY()
+	FTimerHandle LevelLoadStartTimeHandle;
+	UPROPERTY()
+	FTimerHandle ProgressCheckTimeHandle;
+	UPROPERTY()
+	FTimerHandle OpenLevelTimeHandle;
+	
+	UFUNCTION(BlueprintCallable, Category = "LoadLevel")
+	void LoadNextLevelByAsync(const FString& NextLevelPath);	//NextLevelPath : /Game/Level/TopDownSampleScene
+
+	FString NextLevel;
+	bool IsDoneCheckReady = false;
+	
+	UPROPERTY(BlueprintReadOnly)
+	float ProgressLoad;
+
+	TObjectPtr<class UUPLoadLevelUserWidget> LevelLoadWidget;
+
+private :
+	
+	UFUNCTION()
+	void OpenLevel();
+
+	UFUNCTION()
+	void ProgressCheck();
+	
+	UPROPERTY()
+	TObjectPtr<class UWidgetComponent> LevelLoadWidgetComp;
+
+};
