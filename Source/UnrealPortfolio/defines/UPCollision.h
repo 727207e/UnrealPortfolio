82c0717b9c184723317d8aad7c589e6c5d15ae75
Yumin <potapu51@gmail.com>
2024-05-01 17:49:59 +0900

    콜라이더 수정

--- Source/UnrealPortfolio/defines/UPCollision.h
+++ Source/UnrealPortfolio/defines/UPCollision.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #define OVERLAP_ONLY_PAWN TEXT("OverlapOnlyPawn")
-#define CPROFILE_UP_TRACE TEXT("UP_Trace_Profile")
-#define CPROFILE_PAWN TEXT("Pawn")
-#define CPROFILE_UP_NPC TEXT("UP_NPC_Profile")
+#define CPROFILE_UP_TRACE TEXT("PlayerMeetNPC")
+#define CPROFILE_UP_NPC TEXT("NPCMeetPlayer")
 #define CHANNEL_UPTRACE ECC_GameTraceChannel1 
