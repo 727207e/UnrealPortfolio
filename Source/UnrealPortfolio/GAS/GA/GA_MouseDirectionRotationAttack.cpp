5eeb73d065ba6c4314a334040aa5666e888f5bdc
kimdalho <snail0513@naver.com>
2024-05-12 23:22:44 -0700

    1. AbilityTask_LookAtMouse 생성 2. MouseDirectionRotationAttack 생성

---
+++ Source/UnrealPortfolio/GAS/GA/GA_MouseDirectionRotationAttack.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GAS/GA/GA_MouseDirectionRotationAttack.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Data/DataAttributeSet/EntityAttributeSet.h"
+#include "GAS/AbilityTask/AbilityTask_LookAtMouse.h"
+
+class UAbilityTask_LookAtMouse;
+class UEntityAttributeSet;
+
+auto UGA_MouseDirectionRotationAttack::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                                       const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                                       const FGameplayEventData* TriggerEventData) -> void
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	UAbilityTask_LookAtMouse * AttackTraceTask = UAbilityTask_LookAtMouse::CreateTask(this);
+	AttackTraceTask->ReadyForActivation();
+}
+
+
+void UGA_MouseDirectionRotationAttack::CheckComboInput()
+{
+	Super::CheckComboInput();
+	UAbilityTask_LookAtMouse * AttackTraceTask = UAbilityTask_LookAtMouse::CreateTask(this);
+	AttackTraceTask->ReadyForActivation();
+}
