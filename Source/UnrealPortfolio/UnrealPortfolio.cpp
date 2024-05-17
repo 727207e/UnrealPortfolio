b0bd4e17c993aff864ad295b0efee9caa5691e91
Yumin <potapu51@gmail.com>
2024-05-17 16:25:40 +0900

    서버 로그 기능 추가

--- Source/UnrealPortfolio/UnrealPortfolio.cpp
+++ Source/UnrealPortfolio/UnrealPortfolio.cpp
@@ -1,9 +1,11 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "UnrealPortfolio.h"
+#include "defines/UPServerLogDefine.h"
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealPortfolio, "UnrealPortfolio" );
 
+DEFINE_LOG_CATEGORY(LogUPNetwork)
 DEFINE_LOG_CATEGORY(LogUnrealPortfolio)
  
